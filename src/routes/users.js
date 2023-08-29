1f4feca3631852d62053c7aa08a2b883219c2c77
Franklin Sanchez <booba302@gmail.com>
2023-08-29 14:34:15 -0400

    login

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,10 @@ import { isLogged, protectView } from "../utils/secure.middleware.js";
 const userMng = new UserManager();
 const userRouter = Router();
 
+userRouter.get("/", isLogged, async (req, res) => {
+  res.redirect("/login");
+});
+
 userRouter.post("/login", isLogged, async (req, res) => {
   const { email, password } = req.body;
 
